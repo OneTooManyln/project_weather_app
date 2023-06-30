8321873a68b19b9f88bd1bbe1845a400a7bd2b41
OneTooMany <santosismael.115@gmail.com>
2023-06-30 14:56:43 -0700

    Add listener to form to get data with input
    getWeatherData will be called with search input value on submit

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";(async a=>{const o=await fetch("https://api.weatherapi.com/v1/current.json?key=47ac169af086415e85a163023231706&q=London",{mode:"cors"}),t=await o.json();console.log(t)})()})();
+(()=>{"use strict";const e=async e=>{const t=await fetch(`https://api.weatherapi.com/v1/current.json?key=47ac169af086415e85a163023231706&q=${e}`,{mode:"cors"}),o=await t.json();console.log(o)},t=document.querySelector("form"),o=document.querySelector("#search");e("London"),t.addEventListener("submit",(t=>{t.preventDefault(),e(o.value)}))})();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import { getWeatherData } from "./apiFunctions";
 
+const form = document.querySelector("form");
+const searchInputValue = document.querySelector("#search");
+
 getWeatherData("London");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeatherData(searchInputValue.value);
+});
