c3e415b98438d20913fd69ae4a913b66ae7dc7c0
OneTooMany <santosismael.115@gmail.com>
2023-06-30 13:19:39 -0700

    Add parameter to function to fetch data with

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";(async()=>{const a=await fetch("https://api.weatherapi.com/v1/current.json?key=47ac169af086415e85a163023231706&q=london",{mode:"cors"}),o=await a.json();console.log(o.current)})()})();
+(()=>{"use strict";(async a=>{const o=await fetch("https://api.weatherapi.com/v1/current.json?key=47ac169af086415e85a163023231706&q=London",{mode:"cors"}),t=await o.json();console.log(t)})()})();
--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -1,8 +1,8 @@
-export const getWeatherData = async () => {
+export const getWeatherData = async (location) => {
   const response = await fetch(
-    "https://api.weatherapi.com/v1/current.json?key=47ac169af086415e85a163023231706&q=london",
+    `https://api.weatherapi.com/v1/current.json?key=47ac169af086415e85a163023231706&q=${location}`,
     { mode: "cors" }
   );
   const currentData = await response.json();
-  console.log(currentData.current);
+  console.log(currentData);
 };
