b7a53e13406b45213e2e55c2b4c9a65b340fcc96
OneTooMany <santosismael.115@gmail.com>
2023-07-01 10:29:23 -0700

    Fetch all necessary data to render weather info

--- src/domFunctions.js
+++ src/domFunctions.js
@@ -1,4 +1,21 @@
 export const renderWeatherData = (data) => {
-  const city = data.location;
+  const city = data.location.name;
+  const { country } = data.location;
+  const temperature = data.current.temp_c;
+  const windSpeed = data.current.wind_kph;
+  const windDirection = data.current.wind_dir;
+  const { humidity } = data.current;
+  const cloudCoverage = data.current.cloud;
+  const { condition } = data.current;
+  const localTime = data.location.localtime;
+
   console.log(city);
+  console.log(country);
+  console.log(temperature);
+  console.log(windSpeed);
+  console.log(windDirection);
+  console.log(humidity);
+  console.log(cloudCoverage);
+  console.log(condition);
+  console.log(localTime);
 };
