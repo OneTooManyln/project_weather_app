76ecfa122903af05c92885492fd67166733d253f
OneTooMany <santosismael.115@gmail.com>
2023-07-02 14:53:58 -0700

    Import & call renderWorldWeatherInfo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { getWeatherData } from "./apiFunctions";
-import { renderWeatherData } from "./domFunctions";
+import { renderWeatherData, renderWorldWeatherInfo } from "./domFunctions";
 
 const form = document.querySelector("form");
 const searchInputValue = document.querySelector("#search");
@@ -7,6 +7,7 @@ const searchInputValue = document.querySelector("#search");
 // getWeatherData("Seoul");
 // call function with return value from getWeatherData
 renderWeatherData(await getWeatherData("seoul"));
+renderWorldWeatherInfo();
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
