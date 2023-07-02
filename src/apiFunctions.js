32ee109e21f5bd1dbc97893fb9c301f293b4c707
OneTooMany <santosismael.115@gmail.com>
2023-07-02 12:38:47 -0700

    Rmv call to function & return response.json()

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -1,11 +1,10 @@
-import { renderWeatherData } from "./domFunctions";
-
 export const getWeatherData = async (location) => {
   const response = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=47ac169af086415e85a163023231706&q=${location}`,
     { mode: "cors" }
   );
   const currentData = await response.json();
-  console.log(currentData);
-  renderWeatherData(currentData);
+  /* console.log(currentData); */
+  /* renderWeatherData(currentData); */
+  return currentData;
 };
