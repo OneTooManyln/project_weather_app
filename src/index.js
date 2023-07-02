02ff2619a7ef240e784b99596564d7990dc1bc0a
OneTooMany <santosismael.115@gmail.com>
2023-07-02 12:41:31 -0700

    Call dom function wth return value from api func

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { getWeatherData } from "./apiFunctions";
+import { renderWeatherData } from "./domFunctions";
 
 const form = document.querySelector("form");
 const searchInputValue = document.querySelector("#search");
 
-getWeatherData("Seoul");
+// getWeatherData("Seoul");
+// call function with return value from getWeatherData
+renderWeatherData(await getWeatherData("seoul"));
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
